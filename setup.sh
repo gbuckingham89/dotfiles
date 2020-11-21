d7a7a9c7635171df664b6578c856e00393d6449f
George Buckingham <george@georgebuckingham.com>
2020-11-21 13:51:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "Setting up George's Mac..."
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+cp $HOME/.zshrc $HOME/.zshrc.backup
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
